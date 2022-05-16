90a26486ef277d3118930a69f452725a38338198
DMDuran <92621818+DMDuran@users.noreply.github.com>
2022-05-15 21:04:33 -0500

    Added some comments to help clarify structure.  Add withdraw method as will as a getPreviousTransaction method.  This will run some if loops to find whether to print withdraw or deposit.

--- BankingApp/src/BankingApplication.java
+++ BankingApp/src/BankingApplication.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 /**
  * 
  */
@@ -10,13 +11,12 @@ import java.util.Scanner;
 public class BankingApplication {
 
 	/*
-	 * Create classes that will perform functions for the main method
-	 * Deposit, withdraw, 
+	 * Create classes that will perform functions for the main method Deposit,
+	 * withdraw, show previous transactions
 	 */
-	
+
 	public static void main(String[] args) {
 
-		
 	}
 
 	// Class for BankAccount
@@ -36,5 +36,33 @@ public class BankingApplication {
 			}
 		}
 
+		// Creating method that will perform a withdraw functions
+		void withdraw(int amount) {
+			if (amount != 0) // Condition will run if amount given is not 0
+			{
+				balance = balance - amount; // Subtracting amount user gave to account balance
+				previousTransaction = -amount; // Initialzing previousTransaction to match amount wtih -
+			}
+		}
+
+		// Creating method that will perform a print for previous transaction
+		void getPreviousTransaction() {
+			if (previousTransaction > 0) // If condition to check if there was a postive amount
+			{
+				System.out.println("Deposited: " + previousTransaction); // If positive, print amount
+			} else if (previousTransaction < 0) // Else if condition to check if there was a negative amount
+			{
+				System.out.println("Withdraw: " + Math.abs(previousTransaction)); // If negative, print amount.  Math.abs does not show negative
+
+			} else // Else condition will happen if no previousTransaction has been made
+			{
+				System.out.println("No transaction occured.");
+			}
+		}
+		
+		void showMenu()
+		{
+			
+		}
 	}
 }
