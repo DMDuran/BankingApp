15aefb637ee22ab77f02a169a6f2293028ffc369
DMDuran <92621818+DMDuran@users.noreply.github.com>
2022-05-16 01:30:22 -0500

    Finished the show menu and created some loops within. Loops will be a do while and a case switch.  The user will be prompted with letter options for choosing to navigate the app.

--- BankingApp/src/BankingApplication.java
+++ BankingApp/src/BankingApplication.java
@@ -12,7 +12,8 @@ public class BankingApplication {
 
 	/*
 	 * Create classes that will perform functions for the main method Deposit,
-	 * withdraw, show previous transactions
+	 * withdraw, show previous transactions, display a welcome message, and a show
+	 * menu.
 	 */
 
 	public static void main(String[] args) {
@@ -27,12 +28,18 @@ public class BankingApplication {
 		String customerName;
 		String customerId;
 
+		// Creating a constructor to display welcome message
+		BankAccount(String cname, String cid) {
+			customerName = cname;
+			customerId = cid;
+		}
+
 		// Creating first method that will perform deposit functions
 		void deposit(int amount) {
 			if (amount != 0) // Condition will run if amount given is not 0
 			{
 				balance = balance + amount; // Adding amount user gave to account balance
-				previousTransaction = amount; // Initialzing previousTransaction to match amount
+				previousTransaction = amount; // Initializing previousTransaction to match amount
 			}
 		}
 
@@ -41,7 +48,7 @@ public class BankingApplication {
 			if (amount != 0) // Condition will run if amount given is not 0
 			{
 				balance = balance - amount; // Subtracting amount user gave to account balance
-				previousTransaction = -amount; // Initialzing previousTransaction to match amount wtih -
+				previousTransaction = -amount; // Initializing previousTransaction to match amount wtih -
 			}
 		}
 
@@ -52,17 +59,80 @@ public class BankingApplication {
 				System.out.println("Deposited: " + previousTransaction); // If positive, print amount
 			} else if (previousTransaction < 0) // Else if condition to check if there was a negative amount
 			{
-				System.out.println("Withdraw: " + Math.abs(previousTransaction)); // If negative, print amount.  Math.abs does not show negative
+				System.out.println("Withdraw: " + Math.abs(previousTransaction)); // If negative, print amount. Math.abs
+																					// does not show negative
 
 			} else // Else condition will happen if no previousTransaction has been made
 			{
 				System.out.println("No transaction occured.");
 			}
 		}
-		
-		void showMenu()
-		{
-			
+
+		void showMenu() {
+
+			char option = '\0'; // Initializing this values with any value
+			Scanner scn = new Scanner(System.in); // Creating scanner
+
+			System.out.println("Welcome " + customerName);
+			System.out.println("Your ID is " + customerId);
+			System.out.println(); // Line break
+
+			// There print lines will be the options the user can access
+			System.out.println("A. Check Balance");
+			System.out.println("B. Deposit");
+			System.out.println("C. Withdraw");
+			System.out.println("D. Previous Transaction");
+			System.out.println("E. Exit");
+
+			// Performing a do while loop for customer options
+			do {
+				System.out.println("=========================================================================");
+				System.out.print("Enter an option: "); // Prompting user input for which option
+				System.out.println("=========================================================================");
+				option = scn.next().charAt(0); // Char varible will be used to get input
+				System.out.println(); // Line Break
+
+				// Using a case switch loop so for options A, B, C, D, and E.
+				switch (option) // Will read option char which was user input
+				{
+				case 'A': // This case will display balance of account
+					System.out.println("------------------------------------------------------------------");
+					System.out.println("Balance = " + balance);
+					System.out.println("------------------------------------------------------------------");
+					break; // Breaking case
+
+				case 'B': // This case will prompt user for input on how much they would like to deposit
+					System.out.println("------------------------------------------------------------------");
+					System.out.print("Enter amount you would like to deposit: ");
+					System.out.println("------------------------------------------------------------------");
+					int amount = scn.nextInt(); // User scanner for amount
+					deposit(amount); // Calling deposit method
+					System.out.println(); // Line Break
+					break; // Breaking case
+
+				case 'C': // This will prompt user for input on how much they would like to withdraw
+					System.out.println("------------------------------------------------------------------");
+					System.out.print("Enter amount you would like to withdraw: ");
+					System.out.println("------------------------------------------------------------------");
+					int amountTwo = scn.nextInt(); // User scanner for amount
+					withdraw(amountTwo); // Calling withdraw method and printing
+					System.out.println(); // Line Break
+					break; // Breaking case
+
+				case 'D': // This will print previous transaction by calling method
+					System.out.println("------------------------------------------------------------------");
+					getPreviousTransaction(); // Calling method to print
+					System.out.println("------------------------------------------------------------------");
+					break; // Line Break
+
+				default: // If no option was given within the A, B, C, D, E parameters then you will
+							// recieve an error
+					System.out.println("Invalid option!  Please enter again.");
+
+				}
+			} while (option != 'E'); // While condition. If E is selected the case switch loop will break
+			System.out.println("Thank you for using our services!"); // Closing statement for app
+
 		}
 	}
 }
